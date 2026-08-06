71dcc282cdf4e88aff10a69d88624bca45072a8a
André Brandão <andrebrandao@MacBook-Pro-de-Andre.local>
2026-08-06 17:29:50 -0300

    Criação da estrutura da Fila

--- fila.h
+++ fila.h
@@ -3,6 +3,20 @@
 
 #include <stdio.h>
 
+typedef int TipoItem;   // define o tipo de dado dos elementos da fila
+
+// Estrutura do nó
+struct no {
+    TipoItem info;
+    struct no* prox;
+};
+
+// Estrutura da fila
+typedef struct {
+    struct no* inicio;
+    struct no* fim;
+} Fila;
+
 
 
 #endif /* fila_h */
